59c6c530970034199819b96d187940e032e666f9
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2020-09-16 11:22:36 +0200

    Fixed dependencies

--- interview-base/build.gradle.kts
+++ interview-base/build.gradle.kts
@@ -24,27 +24,4 @@ android {
     }
 }
 
-dependencies {
-    api("androidx.appcompat:appcompat:1.1.0")
-    api("androidx.core:core-ktx:1.3.1")
-    api("androidx.recyclerview:recyclerview:1.1.0")
-    api("androidx.cardview:cardview:1.0.0")
-    api("androidx.constraintlayout:constraintlayout:1.1.3")
-    api("androidx.constraintlayout:constraintlayout-solver:1.1.3")
-    api("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-
-    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
-    api("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
-    api("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
-
-    api("androidx.fragment:fragment-ktx:1.1.0")
-
-    api("com.google.android.material:material:1.2.0")
-
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.8")
-
-    api("io.reactivex.rxjava2:rxjava:2.2.19")
-    api("io.reactivex.rxjava2:rxandroid:2.1.1")
-    api("io.reactivex.rxjava2:rxkotlin:2.4.0")
-}
+apply(from = "$rootDir/dependencies.gradle.kts")
